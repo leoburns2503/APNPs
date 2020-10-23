e6c09c0c2dd52b23793e42abf17bd18d5c84696e
leonardo <leonardo.bfmc@gmail.com>
2020-10-23 17:50:30 -0300

    Arquivo ./index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+
+app.get('/', function(req,res){
+    res.send("Olá Leonardo!!")
+})
+
+app.listen(3000,function(){
+    console.log("Conexão inicializada na porta 3000")
+});
