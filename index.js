19d373c0c34d72c68ddb32e63839793113aa27a0
leonardo <leonardo.bfmc@gmail.com>
2020-11-13 01:56:50 -0300

    usando ejs nas paginas

--- index.js
+++ index.js
@@ -14,7 +14,16 @@ app.set("view engine", "ejs")
 app.use(express.static(path.join(__dirname,"public")))
 
 app.get('/', function(req,res){
-    res.send("Olá Leonardo!!")
+    res.render('index.ejs', {})
+})
+
+app.get('/usuarios', function(req,res){
+    res.render('usuarios.ejs', { usuarios:[
+        {nome:'Leonardo',email:'leonardo.bfmc@gmail.com'},
+        {nome:'Ana',email:'Ana@gmail.com'},
+        {nome:'Raquel',email:'Raquel@gmail.com'},
+        {nome:'Maria',email:'Maria@gmail.com'},
+    ]})
 })
 
 app.listen(3000,function(){
